91b2957805a9b7cf8ce04addc2092fb64503c8bc
Xavier Idrovo <xidrovo@hotmail.com>
2018-08-22 16:20:54 -0400

    uppsiiii

--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,14 +4,20 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("")
-def step_impl(context):
-	
+@given("un '{sexo}', que sea '{estado_civil}' de {edad} años, de '{ciudad}' que sufre '{especial}' y con '{dependientes}' hijos")
+def step_impl(context, ciudad, edad, sexo, estado_civil, especial, dependientes):
+	context.sexo 			= sexo
+	context.estado_civil 	= estado_civil
+	context.edad 			= int(edad)
+	context.ciudad 			= ciudad
+	context.especial 		= especial
+	context.dependientes 	= int(dependientes) 
 
-@when("")
+@when("hago el cálculo de la póliza")
 def step_impl(context):
-	
+	resultado = cotizar_seguro(context.ciudad,context.edad,context.sexo,context.estado_civil,context.especial,context.dependientes)
+	context.resultado = resultado
 
-@then("")
+@then("el mensaje enviado es '{total}'")
 def step_impl(context,total):
-	assert 
+	assert context.resultado == total
