ed3496556d9b863a6ce28ddb5535da2dd5582e34
rudmary <moncay.ruddy@gmail.com>
2018-08-22 15:31:23 -0500

    tengo problema con la version de python

--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,14 +4,20 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("")
-def step_impl(context):
-	
+@given("La persona es un '{sexo}', '{estado_civil}' de {edad} años, procedente de '{ciudad}' con '{preexistencia}'")
+def step_impl(context, sexo, estado_civil, edad, ciudad, preexistencia):
+	context.sexo = sexo
+	context.estado_civil = estado_civil
+	context.edad = (edad)
+	context.ciudad = ciudad
+	context.preexistencia = preexistencia
+	context.dependientes = 0
 
-@when("")
+@when("hago el cálculo de la póliza")
 def step_impl(context):
-	
+	resultado = cotizar_seguro(context.ciudad,context.edad,context.sexo,context.estado_civil,context.preexistencia,context.dependientes)
+	context.resultado = resultado
 
-@then("")
+@then("el mensaje enviado es '{total}'")
 def step_impl(context,total):
-	assert 
+	assert context.resultado == total
--- src/cotizador.py
+++ src/cotizador.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+
 def calcular_cuota_basica(dependientes):
 	"""Calcula la cuota basica del asegurado, 
 	dependiendo de su numero de dependientes. 
