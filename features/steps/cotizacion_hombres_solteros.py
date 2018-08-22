e91b6197449473f17a00993247f48db3807ee37a
Arelys Vintimilla <aremvint@espol.edu.ec>
2018-08-22 15:22:09 -0500

    cambios

--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,14 +4,23 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("")
+@given("un conjunto de clientes")
 def step_impl(context):
+	lista_clientes = []
+	for row in context.table:
+		cliente = Clientes(row['CIUDAD'], row['EDAD'],row['SEXO'],
+			row['ESTADO CIVIL'],row['ESPECIAL'],row['DEPENDIENTES'])
+		lista_clientes.append(cliente)
+	context.libros = lista_clientes
 	
 
-@when("")
+@when("busque los clientes hombres solteros")
 def step_impl(context):
+	resultado, mensaje = get_clientes(context.clientes, context.busqueda)
+	context.resultado = resultado
+	context.mensaje = mensaje
 	
 
-@then("")
-def step_impl(context,total):
-	assert 
+@then("obtiene el mensaje '{mensaje}'")
+def step_impl(context,mensaje):
+	assert context.mensaje == mensaje
