062678db79a5747cdf9eb57ba7104262a931db19
Bryan Tumbaco Moreira <bryandario1994@gmail.com>
2018-08-22 15:40:57 -0500

    Update cotizacion_hombres_solteros.py

--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,20 +4,28 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("un '{sexo}', que sea '{estado_civil}' de {edad} años, de '{ciudad}' que sufre '{especial}' y con '{dependientes}' hijos")
-def step_impl(context, ciudad, edad, sexo, estado_civil, especial, dependientes):
-	context.sexo 			= sexo
-	context.estado_civil 	= estado_civil
-	context.edad 			= int(edad)
-	context.ciudad 			= ciudad
-	context.especial 		= especial
-	context.dependientes 	= int(dependientes) 
+@given("un {sexo} de {edad} años que vive en '{ciudad}'' y su estado civil es '{estado_civil}'"
+	   ", con {dependientes} dependientes y padece '{especial}'")
+def step_impl(context, ciudad, dependientes, especial, ciudad, estado_civil, edad, sexo):
+	context.sexo = sexo
+	context.edad  = int(edad)
+	context.ciudad = ciudad
+	context.estado_civil = estado_civil
+	context.especial = especial
+	context.dependientes = int(dependientes)
+	
 
-@when("hago el cálculo de la póliza")
+@when("cotiza el valor del seguro")
 def step_impl(context):
-	resultado = cotizar_seguro(context.ciudad,context.edad,context.sexo,context.estado_civil,context.especial,context.dependientes)
-	context.resultado = resultado
+	mensaje = cotizar_seguro(context.ciudad,
+		                     context.edad,
+		                     context.sexo,
+		                     context.estado_civil,
+		                     context.especial,
+		                     context.dependientes)
+	context.resultado = mensaje
 
-@then("el mensaje enviado es '{total}'")
-def step_impl(context,total):
-	assert context.resultado == total
+
+@then("Obtendrá el mensaje: '{mensaje}'")
+def step_impl(context, mensaje):
+	assert context.mensaje == mensaje
