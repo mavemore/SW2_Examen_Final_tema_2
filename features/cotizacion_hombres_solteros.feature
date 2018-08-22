1a6be7b5b7821f98197838581da461b37d4d6f46
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-08-22 15:31:55 -0500

    prueba para caso 1

--- features/cotizacion_hombres_solteros.feature
+++ features/cotizacion_hombres_solteros.feature
@@ -3,7 +3,7 @@
 Característica: Obtener cotización para clientes hombres solteros.
 
   @hombres_solteros
-  Escenario: 
-      Dado 
-      Cuando 
-      Entonces 
+  Escenario: Un hombre adulto soltero, con ningun dependiente ni condicion especial, de Cuenca, solicita una cotizacion de seguro a Saludcita
+      Dada que un 'hombre' tiene 18 años ademas tiene 0 dependientes ademas es de 'Cuenca' ademas es 'soltero' ademas tiene 'ninguna' condicion especial
+      Cuando cotiza su seguro
+      Entonces obtiene el mensaje 'El valor calculado de su cotización es de 70.00'
--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,14 +4,28 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("")
-def step_impl(context):
-	
+@given(u'que un \'{sexo}\' tiene {edad} años ademas tiene {dependientes} dependientes ademas es de \'{ciudad}\' ademas es \'{estado_civil}\' ademas tiene \'{especial}\' condicion especial')
+def step_impl(context,sexo,edad,dependientes,especial,estado_civil,ciudad):
+	context.sexo = sexo
+	context.edad = edad
+	context.dependientes = dependientes
+	context.especial = especial
+	context.estado_civil = estado_civil
+	context.ciudad = ciudad
 
-@when("")
+@when("cotiza su seguro")
 def step_impl(context):
-	
+	valor = cotizar_seguro(context.sexo, context.edad, context.sexo, context.estado_civil, context.especial, context.dependientes)
+	context.valor = valor
 
-@then("")
-def step_impl(context,total):
-	assert 
+@then("obtiene el mensaje '{resultado}'")
+def step_impl(context,resultado):
+	print(context.valor)
+	print(resultado)
+	are_equal = context.valor == resultado
+	assert are_equal is True
+
+@given("que un '{sexo}' tiene {edad} años")
+def step_impl(context,sexo,edad):
+	context.sexo = sexo
+	context.edad = edad
