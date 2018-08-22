0551ae9a3a6207255a84e26f43a525a48a3b144f
NarcisaC <ncolcha@espol.edu.ec>
2018-08-22 15:59:42 -0500

    Tema 2

--- features/cotizacion_hombres_solteros.feature
+++ features/cotizacion_hombres_solteros.feature
@@ -2,8 +2,37 @@
 
 Característica: Obtener cotización para clientes hombres solteros.
 
+
+
   @hombres_solteros
-  Escenario: 
-      Dado 
-      Cuando 
-      Entonces 
+  Escenario: Solicita cotización de seguro un hombre soltero
+      Dado el usuario es hombre soltero con 20 años
+      Cuando el usuario solicite el seguro y tiene 0 cargas
+      Entonces se mostrará el mensaje con la cotización 'El valor calculado de su cotización es de 70.00"
+
+
+
+  @hombres_solteros
+  Escenario: Solicita cotización de seguro un hombre soltero
+      Dado el usuario es hombre soltero con 20 años
+      Cuando el usuario solicite el seguro y tiene 1 cargas
+      Entonces se mostrará el mensaje con la cotización 'El valor calculado de su cotización es de 70.00"
+
+  @hombres_solteros
+  Escenario: Solicita cotización de seguro un hombre soltero
+      Dado el usuario es hombre soltero con 25 años
+      Cuando el usuario solicite el seguro y tiene 2 cargas
+      Entonces se mostrará el mensaje con la cotización 'El valor calculado de su cotización es de 70.00"
+
+  @hombres_solteros
+  Escenario: Solicita cotización de seguro un hombre soltero
+      Dado el usuario es hombre soltero con 25 años
+      Cuando el usuario solicite el seguro y tiene 3 cargas
+      Entonces se mostrará el mensaje con la cotización 'El valor calculado de su cotización es de 70.00"
+
+
+  @hombres_solteros
+  Escenario: Solicita cotización de seguro un hombre soltero
+      Dado el usuario es hombre soltero con 25 años
+      Cuando el usuario solicite el seguro y tiene 4 cargas
+      Entonces se mostrará el mensaje con la cotización 'El valor calculado de su cotización es de 70.00"
--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,14 +4,107 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("")
-def step_impl(context):
-	
+@given("el usuario es hombre soltero con {num1} años")
+def step_impl(context, num1):
+	resultado = ""
+	if ((int(num1) > 17) and (int(num1) < 41)):
+		posibilidad = True
+		context.resultado = posibilidad
 
-@when("")
-def step_impl(context):
-	
 
-@then("")
-def step_impl(context,total):
-	assert 
+@when("el usuario solicite el seguro y tiene {num2} cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context.dependientes, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+@when("el usuario solicite el seguro y tiene 1 cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context.dependientes, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+@when("el usuario solicite el seguro y tiene 2 cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context.dependientes, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+@when("el usuario solicite el seguro y tiene 3 cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context.dependientes, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+@when("el usuario solicite el seguro y tiene 4 cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context.dependientes, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+
+
+
+@then("se mostrará el mensaje con la cotización '{mensaje}'")
+def step_impl(context, mensaje):
+	mensaje = cotizar_seguro("Guayaquil", 20, "hombre", "soltero", 0)
+	context.mensaje = mensaje
+
+
+
+@given("el usuario es hombre soltero con {num1} años.")
+def step_impl(context, num1):
+	resultado = ""
+	if ((int(num1) > 17) and (int(num1) < 41)):
+		posibilidad = True
+		context.resultado = posibilidad
+
+
+@when("el usuario solicite el seguro y tiene {num2} cargas")
+def step_impl(context, num2):
+	r = False
+	q = calcular_cuota_basica(context, num2)
+	if ((q == num2) & (q > 0)):
+		r = True
+	assert r is True
+
+
+
+
+@then("se mostrará el mensaje con la cotización '{mensaje}'.")
+def step_impl(context, mensaje):
+	mensaje = cotizar_seguro("Guayaquil", 20, "hombre", "soltero", 1)
+	context.mensaje = mensaje
+
+
+@then("se mostrará el mensaje con la cotización '{mensaje}'.")
+def step_impl(context, mensaje):
+	mensaje = cotizar_seguro("Guayaquil", 20, "hombre", "soltero", 2)
+	context.mensaje = mensaje
+
+
+@then("se mostrará el mensaje con la cotización '{mensaje}'.")
+def step_impl(context, mensaje):
+	mensaje = cotizar_seguro("Guayaquil", 20, "hombre", "soltero", 3)
+	context.mensaje = mensaje
+
+
+@then("se mostrará el mensaje con la cotización '{mensaje}'.")
+def step_impl(context, mensaje):
+	mensaje = cotizar_seguro("Guayaquil", 20, "hombre", "soltero", 4)
+	context.mensaje = mensaje
