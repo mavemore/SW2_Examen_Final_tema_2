db9c6ef3625d40f56f70f325d3101df0dfd770ff
Xavier Idrovo <xidrovo@hotmail.com>
2018-08-22 16:20:14 -0400

    Revert "done"
    This reverts commit 2f5c16c50ad26182ae05ba60a35c5dc0d94f6940.

--- features/steps/cotizacion_hombres_solteros.py
+++ features/steps/cotizacion_hombres_solteros.py
@@ -4,20 +4,14 @@ from src.cotizador import *
 def before_scenario(context, scenario):
 	context = {}
 
-@given("un '{sexo}', que sea '{estado_civil}' de {edad} años, de '{ciudad}' que sufre '{especial}' y con '{dependientes}' hijos")
-def step_impl(context, ciudad, edad, sexo, estado_civil, especial, dependientes):
-	context.sexo 			= sexo
-	context.estado_civil 	= estado_civil
-	context.edad 			= int(edad)
-	context.ciudad 			= ciudad
-	context.especial 		= especial
-	context.dependientes 	= int(dependientes) 
-
-@when("hago el cálculo de la póliza")
+@given("")
 def step_impl(context):
-	resultado = cotizar_seguro(context.ciudad,context.edad,context.sexo,context.estado_civil,context.especial,context.dependientes)
-	context.resultado = resultado
+	
 
-@then("el mensaje enviado es '{total}'")
+@when("")
+def step_impl(context):
+	
+
+@then("")
 def step_impl(context,total):
-	assert context.resultado == total
+	assert 
